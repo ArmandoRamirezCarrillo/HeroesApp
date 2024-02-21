1af522167b0d50e7ab4a6058acb4c0024bc99684
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2024-02-20 21:03:47 -0600

    Creando la busqueda

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -17,6 +17,7 @@ export const Navbar = () => {
                 <div className="navbar-nav">
                     <NavLink className={ ({isActive}) => `nav-item nav-link ${isActive?'active':''}`} to="/marvel">Marvel</NavLink>
                     <NavLink className="nav-item nav-link" to="/dc">DC</NavLink>
+                    <NavLink className="nav-item nav-link" to="/search">Search</NavLink>
                 </div>
             </div>
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
