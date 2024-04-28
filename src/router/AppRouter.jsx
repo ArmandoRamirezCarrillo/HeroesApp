a235576f64ac480159aa34f76ad8dbf952b90a2d
Jose Armando Ramirez Carrillo <ing.armandoramirez@outlook.com>
2024-04-28 14:09:27 -0600

    Routes privates and publics

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,16 +1,18 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import { LoginPage } from "../auth";
-import { HeroesRoutes, childHeroesRoutes } from "../heroes/routes";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { LoginPage } from '../auth';
+import { HeroesRoutes, childHeroesRoutes } from '../heroes/routes';
+import { PrivateRouter } from './PrivateRouter';
+import { PublicRouter } from './PublicRouter';
 // import { DcPage, MarvelPage } from "../heroes";
 
 const router = createBrowserRouter([
     {
         path: '/login',
-        element: <LoginPage/>,
+        element: <PublicRouter> <LoginPage/> </PublicRouter>,
     },
     {
         path: '/',
-        element: <HeroesRoutes/>,
+        element: <PrivateRouter> <HeroesRoutes/> </PrivateRouter> ,
         children: childHeroesRoutes
     },
 ]);
